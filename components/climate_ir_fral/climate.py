4dd7c10fb2c542e253b0f84e9b52ae3107b300a5
Triluch <triluch@user.noreply.github.com>
2026-07-05 01:42:14 +0200

    Remove deprecated SCHEMA constant.

--- components/climate_ir_fral/climate.py
+++ components/climate_ir_fral/climate.py
@@ -1,5 +1,4 @@
 import esphome.codegen as cg
-import esphome.config_validation as cv
 from esphome.components import climate_ir
 from esphome.const import CONF_ID
 
@@ -9,9 +8,7 @@ AUTO_LOAD = ["climate_ir"]
 climate_ir_fral_ns = cg.esphome_ns.namespace("climate_ir_fral")
 FralClimate = climate_ir_fral_ns.class_("FralClimate", climate_ir.ClimateIR)
 
-CONFIG_SCHEMA = climate_ir.CLIMATE_IR_WITH_RECEIVER_SCHEMA.extend(
-    {cv.GenerateID(): cv.declare_id(FralClimate)}
-)
+CONFIG_SCHEMA = climate_ir.climate_ir_with_receiver_schema(FralClimate)
 
 
 async def to_code(config):
